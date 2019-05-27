a07d176926d6ac5be7ebb2cf1c3da067c785c5eb
Marcus de Lacerda <dxa@qlik.com>
2019-05-27 13:44:51 +0200

    Updated sha256sum of archive

--- Formula/qlik-corectl.rb
+++ Formula/qlik-corectl.rb
@@ -2,7 +2,7 @@ class QlikCorectl < Formula
   desc "Corectl is a CLI tool for using Qlik Associative Engine"
   homepage "https://github.com/qlik-oss/corectl"
   url "https://github.com/qlik-oss/corectl/archive/v1.1.1.tar.gz"
-  sha256 "032923ed8152b409f9c58a838243137a4c529f8ba41e19de3cb9d03207b2dbda"
+  sha256 "6c5528fd9a4414fcbe21bd0b451a71a6aee6ebad3820fdc9adb0c6a6886ff52c"
 
   depends_on "go" => [:build, "1.11"]
 
