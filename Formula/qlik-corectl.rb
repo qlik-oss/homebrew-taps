03152a7d2f6091ac7406fbf8d3b93c18858ab7fa
Marcus de Lacerda <dxa@qlik.com>
2019-05-27 15:55:56 +0200

    Adding bottles to repo as temp solution

--- Formula/qlik-corectl.rb
+++ Formula/qlik-corectl.rb
@@ -4,6 +4,12 @@ class QlikCorectl < Formula
   url "https://github.com/qlik-oss/corectl/archive/v1.1.1.tar.gz"
   sha256 "e0c33093936218932031cbaa3135efe84506cbb7a4a54dacefda299179381301"
 
+  bottle do
+    root_url "https://github.com/qlik-oss/homebrew-taps/bottles"
+    sha256 "3183bb78b19c0bf964d87b3256f560f8f2878b8a1e8b48b06925723531b1c69e" => :mojave
+    sha256 "4e4b9e1f723a8169d68aac026cb1b2eea1c622654ddf83ad4402223a4432bc00" => :x86_64_linux
+  end
+
   depends_on "go" => [:build, "1.11"]
 
   def install
