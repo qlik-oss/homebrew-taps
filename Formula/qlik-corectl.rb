c144e618586da43240bbfedf49c127b4b31b0660
Marcus de Lacerda <dxa@qlik.com>
2019-06-20 16:19:59 +0200

    Updated corectl formula to 1.2.0

--- Formula/qlik-corectl.rb
+++ Formula/qlik-corectl.rb
@@ -1,14 +1,8 @@
 class QlikCorectl < Formula
   desc "Corectl is a CLI tool for using Qlik Associative Engine"
   homepage "https://github.com/qlik-oss/corectl"
-  url "https://github.com/qlik-oss/corectl/archive/v1.1.1.tar.gz"
-  sha256 "e0c33093936218932031cbaa3135efe84506cbb7a4a54dacefda299179381301"
-
-  bottle do
-    root_url "https://github.com/qlik-oss/homebrew-taps/raw/master/bottles"
-    sha256 "3183bb78b19c0bf964d87b3256f560f8f2878b8a1e8b48b06925723531b1c69e" => :mojave
-    sha256 "4e4b9e1f723a8169d68aac026cb1b2eea1c622654ddf83ad4402223a4432bc00" => :x86_64_linux
-  end
+  url "https://github.com/qlik-oss/corectl/archive/v1.2.0.tar.gz"
+  sha256 "8fe794bcc59c511e8f07d7cb728bb3bce8283fe4fac23f6347a65c28a9a88856"
 
   depends_on "go" => [:build]
 
