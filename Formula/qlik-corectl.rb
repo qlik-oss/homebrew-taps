c7d33c58e83bfd3e077d73523984b906744eb273
Mattias Wennmo <mattias.wennmo@qlik.com>
2019-09-10 12:18:46 +0200

    Update formula with correctl url and sha

--- Formula/qlik-corectl.rb
+++ Formula/qlik-corectl.rb
@@ -1,7 +1,7 @@
 class QlikCorectl < Formula
   desc "Corectl is a CLI tool for using Qlik Associative Engine"
   homepage "https://github.com/qlik-oss/corectl"
-  url "https://github.com/qlik-oss/corectl/archive/v1.3.0.tar.gz"
+  url "https://github.com/qlik-oss/corectl/releases/download/v1.3.0/corectl-Darwin-x86_64.tar.gz"
   sha256 "a5eb168d0bf79671c238557a65cfa882479955c6de2903784e1185a18ab64c40"
 
   def install
