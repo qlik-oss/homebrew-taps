b43e68f86f458698742169e07f6883c101a59ea4
Marcus de Lacerda <dxa@qlik.com>
2020-05-12 15:45:23 +0200

    Update formula

--- Formula/qlik-cli.rb
+++ Formula/qlik-cli.rb
@@ -5,11 +5,11 @@ class QlikCli < Formula
   bottle :unneeded
 
   if OS.mac?
-    url "https://raw.githubusercontent.com/qlik-oss/homebrew-taps/publish-experiments/bin/qlik-cli/v0.3.1/qlik-Darwin-x86_64.tar.gz"
+    url "https://raw.githubusercontent.com/qlik-oss/homebrew-taps/master/bin/qlik-cli/v0.3.1/qlik-Darwin-x86_64.tar.gz"
     sha256 "cd434c29c726ce08125c476b5696923afcdcb4f829e9bffd1b5313cac210981a"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://raw.githubusercontent.com/qlik-oss/homebrew-taps/publish-experiments/bin/qlik-cli/v0.3.1/qlik-Linux-x86_64.tar.gz"
+      url "https://raw.githubusercontent.com/qlik-oss/homebrew-taps/master/bin/qlik-cli/v0.3.1/qlik-Linux-x86_64.tar.gz"
       sha256 "4dc0a88833317b8c71a80ff3cacfbcb995e68f5e0662a31f1417545bd1a41c9a"
     end
   end
