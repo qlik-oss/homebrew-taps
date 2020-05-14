078c88601c0db5d9a3b092d393d73f2fa89d0397
Marcus de Lacerda <dxa@qlik.com>
2020-05-14 14:08:22 +0200

    Make publish.sh more robust

--- publish.sh
+++ publish.sh
@@ -1,14 +1,22 @@
-# Update checks if there are any changes present.
+# !/bin/bash
+
+# Publish checks if there are any changes present.
 # It is meant to be run after 'go run publish.go'
 # and push all the changes from it.
 # If no changes are present, nothing is done.
 
+set -e
+
 publish() {
+  echo "Check for new commits"
+  echo "git pull"
+  git pull
   echo "go run publish.go"
   go run publish.go
-  diff=$(git diff)
+  # Check if any changes were made to the bin or Formula folder.
+  diff=$(git diff bin Formula)
   if [ -z $diff ]; then
-    echo "No changes found, all done."
+    echo "No changes to add, all done."
   else
     echo "Found changes to commit and push"
     echo "git add bin"
